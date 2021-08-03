b1af8e8d2b893f1123aecfe6ec2a9eeb672ba743
Ben Jones <ben.jones2@trustpayments.com>
2021-08-03 14:10:38 +0100

    seems to work

--- mg_ch.py
+++ mg_ch.py
@@ -9,31 +9,64 @@ from models import *
 from funcs import *
 
 
-if __name__ == "__main__":
+def main():
     start_time = time.time() # for timing
-    # read and process
-    site_list = list() 
-    # for all of the selected files
-    for file in sys.argv[2:]:
-        # get a list of all the lines in the log and a list of all the siterefs on v2
-        lines = get_lines(file)
-        new_sites = find_sites(lines)
-        # give each site its iss and list of wrefs
-        site_list += process_sites(new_sites, lines)
+    # Read through logs and process sites as they are found
+    sites = SiteList()
+    for file in sys.argv[1:]:
+        with open(file, "r") as log:
+            while True:
+            # Go line by line through the log file, enumerating
+                line = log.readline()
+                if line == "": 
+                    break
+                pos = log.tell()
+            # if line contains STJSVERSION... 
+                if "STJSVERSION STJS::N/A::2." in line:
+                    # ..and the siteref isn't in sites.get_refs()..
+                    match_siteref = re.search(
+                        "\s([a-z]+[0-9]+|test_[a-z]+[0-9]+)", line)
+                    if match_siteref:
+                        siteref = match_siteref.group(1)
+                        w_ref = get_wref(line)
+                        if sites.does_not_contain(siteref):
+                        #   search from this line onwards in lines that have the above wref (max 30 lines)
+                            for i in range(20):
+                                search_line = log.readline()
+                                if w_ref in search_line:
+                                    match_iss = re.search(
+                                        """(?:'|")iss(?:'|"): u?(?:'|")([^(?:'|")]+)(?:'|")""",
+                                        search_line
+                                    )
+                                    #if iss is found (and is not ppagejwt), add Site to sites with siteref,iss and wref
+                                    if match_iss:
+                                        iss = match_iss.group(1)
+                                        if iss == "ppagejwt":
+                                            break
+                                        new_site = Site(siteref)
+                                        new_site.add_wref(w_ref)
+                                        new_site.iss = iss
+                                        sites.add(new_site)
+                                        print(f"Added {siteref}")
+                                        break
+                            log.seek(pos)
+                        # elif line contains stjsversion and siteref IS in sites.get_refs():
+            #   add wref to Site    
+                        else:
+                            site = sites.get_site(siteref)
+                            site.add_wref(w_ref)
     # select all non ppages sites and count how many transactions they processed
-    results = dict()
-    for site in site_list:
-        if site.iss != "ppagejwt":
-            if site.siteref not in results.keys():
-                results[site.siteref] = len(site.w_refs)
-            else:
-                results[site.siteref] = results[site.siteref] + len(site.w_refs)
-            print(site)
+    results = {}
+    
     # write the results to file
     with open("./migration_output.csv", "w") as file:
         file.write("sitereference,transactions\n")
-        for site in sorted(results.items(), key=lambda s: s[1]):
-            line = f"{site[0]},{site[1]}\n"
+        for site in sites:
+            file.write(f"{site.siteref},{len(site.w_refs)}\n")
     print("*"*40)
     print("Output written to migration_output.csv" + " "*20)
     print(f"Completed in {round(time.time()-start_time)} seconds") # for timing
+
+
+if __name__=="__main__":
+    main()
--- models.py
+++ models.py
@@ -1,20 +1,23 @@
 class Site():
     def __init__(self, siteref):
         self.siteref = siteref
-        self.iss = "not found"
-        self.w_refs = list()
+        self.iss = None
+        self.w_refs = set()
     
     def __repr__(self):
         return f"{self.siteref} [{self.iss}] - {len(self.w_refs)} w-refs"
 
     def add_wref(self, wref: str):
-        self.w_refs.append(wref)
+        self.w_refs.add(wref)
 
 
 
 class SiteList():
     def __init__(self):
         self._sites: dict[str, Site] = dict()
+
+    def __iter__(self):
+        return iter(self._sites.values())
 
     def get_site(self, siteref: str) -> Site:
         return self._sites[siteref]
@@ -25,10 +28,12 @@ class SiteList():
     def does_not_contain(self, siteref: str) -> bool:
         return siteref not in self._sites.keys()
 
-    def get(self) -> dict:
-        """Returns a dict[str, Site]"""
-        return self._sites.items()
+    def get_refs(self) -> list:
+        return self._sites.keys()
 
     def get_total(self) -> int:
         return len(self._sites.keys())
+
+    def as_dict(self) -> dict:
+        return self._sites
 
