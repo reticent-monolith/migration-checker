7699ab2631d93839e98c3650a8bb1792be9d5541
Ben Jones <ben.jones2@trustpayments.com>
2021-08-04 16:04:09 +0100

    more tidying...

--- migration_checker.py
+++ migration_checker.py
@@ -2,18 +2,19 @@
 """
 Arguments are files to process
 """
-from multiprocessing import Pool, Queue
-import sys, time ,re
-from multiprocessing import Process, Queue
+from multiprocessing import Pool
+import sys, time, re
+
 
 class Site():
     def __init__(self, siteref):
         self.siteref = siteref
         self.iss = None
         self.w_refs = set()
-    
+
     def __repr__(self):
-        return f"{self.siteref},{self.iss},{len(self.w_refs)}\n" # to write itself into a csv at the end
+        # to write itself into a csv at the end
+        return f"{self.siteref},{self.iss},{len(self.w_refs)}\n"
 
     def add_wref(self, wref: str):
         self.w_refs.add(wref)
@@ -24,10 +25,12 @@ class Site():
         return self
 
 
-
 class SiteList():
-    def __init__(self):
+    def __init__(self, lists: list = None):
         self._sites: dict[str, Site] = dict()
+        if lists:
+            for sl in lists:
+                self.merge(sl)
 
     def __iter__(self):
         return iter(self._sites.values())
@@ -61,6 +64,8 @@ class SiteList():
                 site = self.get_site(new_site.siteref)
                 site.w_refs |= new_site.w_refs
 
+
+
 def get_wref(line):
     match = re.search(
         "(W\d{2}-[0-9a-zA-Z]{8})",
@@ -72,22 +77,6 @@ def get_wref(line):
         # 1/0
         return ""
 
-def main():
-    start_time = time.time() # for timing
-    # Read through logs and process sites as they are found
-    pool = Pool(processes=6)
-    sites = pool.map(process_file, sys.argv[1:])
-    results = SiteList()
-    print("Merging SiteLists...")
-    for s in sites:
-        results.merge(s)
-    # write the results to file
-    with open("./migration_output.csv", "w") as file:
-        file.write("sitereference,username,transactions\n")
-        for site in results:
-            file.write(str(site))
-    print("Output written to migration_output.csv" + " "*20)
-    print(f"Completed in {round(time.time()-start_time)} seconds") 
 
 def process_file(file) -> SiteList:
     print(f"Processing {file}")
@@ -96,13 +85,13 @@ def process_file(file) -> SiteList:
         while True:
             # Go line by line through the log file, enumerating
             line = log.readline()
-            if line == "": 
+            if line == "":
                 break
-            # if line contains STJSVERSION... 
+            # if line contains STJSVERSION...
             if "STJSVERSION STJS::N/A::2." in line:
-                    # ..and the siteref isn't in sites.get_refs()..
+                # ..and the siteref isn't in sites.get_refs()..
                 match_siteref = re.search(
-                        "\s([a-z]+[0-9]+|test_[a-z]+[0-9]+)", line)
+                    "\s([a-z]+[0-9]+|test_[a-z]+[0-9]+)", line)
                 if match_siteref:
                     siteref = match_siteref.group(1)
                     w_ref = get_wref(line)
@@ -113,10 +102,10 @@ def process_file(file) -> SiteList:
                             search_line = log.readline()
                             if w_ref in search_line:
                                 match_iss = re.search(
-                                        """(?:'|")iss(?:'|"): u?(?:'|")([^(?:'|")]+)(?:'|")""",
-                                        search_line
-                                    )
-                                    #if iss is found (and is not ppagejwt), add Site to sites with siteref,iss and wref
+                                    """(?:'|")iss(?:'|"): u?(?:'|")([^(?:'|")]+)(?:'|")""",
+                                    search_line
+                                )
+                                # if iss is found (and is not ppagejwt), add Site to sites with siteref,iss and wref
                                 if match_iss:
                                     iss = match_iss.group(1)
                                     if iss == "ppagejwt":
@@ -127,11 +116,19 @@ def process_file(file) -> SiteList:
                                     break
                         log.seek(pos)
                         # elif line contains stjsversion and siteref IS in sites.get_refs():
-                        #   add wref to Site    
+                        #   add wref to Site
                     else:
                         site = sites.get_site(siteref)
                         site.add_wref(w_ref)
     return sites
 
-if __name__=="__main__":
-    main()
+
+if __name__ == "__main__":
+    # Read through logs and process sites as they are found
+    results = SiteList(Pool(processes=6).map(process_file, sys.argv[1:]))
+    # write the results to file
+    with open("./migration_output.csv", "w") as file:
+        file.write("sitereference,username,transactions\n")
+        for site in results:
+            file.write(str(site))
+    print("Output written to migration_output.csv")
